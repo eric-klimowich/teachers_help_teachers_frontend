d8ac5f0cdba91ea3134abcc738e9ae798a63c854
eric-klimowich <eklimowich@hotmail.com>
2019-01-30 14:55:27 -0500

    Setting initial state in React for users to begin building page for selected user

--- src/UserContainer.js
+++ src/UserContainer.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react'
 
 class UserContainer extends Component {
+
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/users')
+      .then(r => r.json())
+      .then(users => this.setState({ users }))
+  }
+
   render() {
+    console.log(this.state.users)
     return <div>In UserContainer</div>
   }
 }
