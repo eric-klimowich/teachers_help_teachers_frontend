6e61ad349208e0fdead3f675e9522e503966a318
eric-klimowich <eklimowich@hotmail.com>
2019-01-30 15:33:46 -0500

    Temporary functionality built to select user

--- src/UserContainer.js
+++ src/UserContainer.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 class UserContainer extends Component {
 
   state = {
-    users: []
+    users: [],
+    value: "1",
+    currentUser: null
   }
 
   componentDidMount() {
@@ -12,9 +14,27 @@ class UserContainer extends Component {
       .then(users => this.setState({ users }))
   }
 
+  selectUser = () => {
+    this.setState({
+      currentUser: [...this.state.users].find(user => user.id === parseInt(this.state.value))
+    })
+  }
+
+  handleChange = event => {
+    this.setState({
+      value: event.target.value
+    }, () => this.selectUser())
+  }
+
   render() {
-    console.log(this.state.users)
-    return <div>In UserContainer</div>
+    console.log(this.state.currentUser)
+    return (
+      <form>
+        <select value={this.state.value} onChange={this.handleChange} >
+          {this.state.users.map(user => <option key={user.id} value={user.id}>{user.first_name}</option>)}
+        </select>
+      </form>
+    )
   }
 }
 
