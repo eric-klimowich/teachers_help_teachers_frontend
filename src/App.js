29b6432384c4d5bf23afd4802d7ea6fe6c22461e
eric-klimowich <eklimowich@hotmail.com>
2019-01-31 16:11:49 -0500

    Added redux and react-redux, created store, and added users to global state

--- src/App.js
+++ src/App.js
@@ -1,61 +1,80 @@
 import React, { Component } from 'react';
 import './App.css';
 import UserContainer from './UserContainer'
+import { connect } from 'react-redux'
+import { setUsers } from './actions'
 
 class App extends Component {
-
-  state = {
-    users: [],
-    lessons: [],
-    comments: [],
-    currentUser: null
-  }
 
   componentDidMount() {
     fetch('http://localhost:3000/api/v1/users')
       .then(r => r.json())
-      .then(users => this.setState({ users }))
+      .then(users => this.props.setUsers(users))
 
-    fetch('http://localhost:3000/api/v1/lessons')
-      .then(r => r.json())
-      .then(lessons => this.setState({ lessons }))
-
-    fetch('http://localhost:3000/api/v1/comments')
-      .then(r => r.json())
-      .then(comments => this.setState({ comments }))
+    // fetch('http://localhost:3000/api/v1/lessons')
+    //   .then(r => r.json())
+    //   .then(lessons => console.log(lessons))
+    //
+    // fetch('http://localhost:3000/api/v1/comments')
+    //   .then(r => r.json())
+    //   .then(comments => this.setState({ comments }))
   }
 
-  handleSelectedUser = event => {
-    this.setState({
-      currentUser: [...this.state.users].find(user => user.id === parseInt(event.target.value))
-    })
-  }
+  // handleSelectedUser = event => {
+  //   this.setState({
+  //     currentUser: [...this.state.users].find(user => user.id === parseInt(event.target.value))
+  //   })
+  // }
 
-  handleIncreaseTimesUsed = (id) => {
-    // console.log('clicked')
-    console.log(id)
-    const lessonsCopy = [...this.state.lessons]
-    const lessonToChange = lessonsCopy.find(lesson => lesson.id === id)
-    console.log(lessonToChange)
-    lessonToChange.times_used = lessonToChange.times_used + 1
-    this.setState({ lessons: lessonsCopy })
-  }
+  // handleIncreaseTimesUsed = (id) => {
+  //   // console.log('clicked')
+  //   console.log(id)
+  //   const lessonsCopy = [...this.state.lessons]
+  //   const lessonToChange = lessonsCopy.find(lesson => lesson.id === id)
+  //   console.log(lessonToChange)
+  //   lessonToChange.times_used = lessonToChange.times_used + 1
+  //   this.setState({ lessons: lessonsCopy })
+  // }
 
   render() {
+    console.log(this.props)
     return (
       <div>
         In App
-        <UserContainer
-          users={this.state.users}
-          lessons={this.state.lessons}
-          comments={this.state.comments}
-          handleSelectedUser={this.handleSelectedUser}
-          currentUser={this.state.currentUser}
-          handleIncreaseTimesUsed={this.handleIncreaseTimesUsed}
-        />
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    users: state.users
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    setUsers: (users) => dispatch(setUsers(users))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+
+
+
+
+// <UserContainer
+// users={this.props.users}
+// />
+
+
+
+
+
+
+// lessons={this.state.lessons}
+// comments={this.state.comments}
+// handleSelectedUser={this.handleSelectedUser}
+// currentUser={this.state.currentUser}
+// handleIncreaseTimesUsed={this.handleIncreaseTimesUsed}
--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,15 @@
+const defaultState = {
+  users: [],
+  lessons: [],
+  comments: [],
+  currentUser: null
+}
 
+export default function (state = defaultState, action) {
+  switch(action.type) {
+    case 'SET_USERS':
+      return {...state, users: action.payload}
+    default:
+      return state
+  }
+}
