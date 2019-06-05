ed3313e058728d36e1a60d456cee0d56360517eb
eric-klimowich <eklimowich@hotmail.com>
2019-06-05 15:10:45 -0400

    Moved LessonsContainer to containers folder, fixed imports

--- src/containers/LessonsContainer.js
+++ src/containers/LessonsContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import LessonsList from './LessonsList'
+import LessonsList from '../LessonsList'
 
 class LessonsContainer extends Component {
 
