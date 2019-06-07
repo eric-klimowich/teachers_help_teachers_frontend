99fa8f89a189ba90d4f7b62fc79d025714fde310
eric-klimowich <eklimowich@hotmail.com>
2019-06-07 15:50:04 -0400

    Fixed nesting issue in Welcome page, updated styling

--- src/Welcome.js
+++ src/Welcome.js
@@ -6,13 +6,11 @@ const Welcome = props => {
       <div id="welcome-intro" className="homepage-introduction__container" >
         <h1 className="homepage-introduction__title">
           Welcome to Teachers Helping Teachers!
-          <br />
-          <br />
-          <h3 className="homepage-introduction__intro" >
+        </h1>
+        <h1 className="homepage-introduction__intro" >
           A way for all teachers, new and experienced, to share and find lesson plan and worksheet samples for free.
           Since we are not only teachers but also lifelong students, helpful feedback is always welcome.
           I hope this serves as a valuable resource to the teaching community.
-          </h3>
         </h1>
       </div>
     </div>
