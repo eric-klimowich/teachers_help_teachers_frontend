249636b131d0c564f49d8ee95824e8aab95356ab
eric-klimowich <eklimowich@hotmail.com>
2019-06-07 16:06:15 -0400

    Changed class name structure in LessonStatistics

--- src/LessonStatistics.js
+++ src/LessonStatistics.js
@@ -9,14 +9,14 @@ class LessonStatistics extends Component {
   render() {
     // console.log(this.props.myLessonsChoice)
     return (
-      <div className="ui container text-container" >
-        <h3 className="chalkboard-writing" >
+      <div className="statistics-container" >
+        <h3 className="statistics-total__community" >
           Our community has <strong>{this.props.lessons.length} lessons</strong> for you to choose from!
           To view all available lessons, click "View All Lessons" above.
           You currently have <strong>{this.props.myFavoriteLessons.length} lessons</strong> saved to your Favorites.
         </h3>
         <Button action={this.props.setMyLessonsChoiceToFavorites} text="Click to View Favorites" />
-        <h3 className="chalkboard-writing" >
+        <h3 className="statistics-total__user" >
           You have currently submitted <strong>{this.props.myLessons.length} lessons</strong> of your own to the community.
         </h3>
         <Button action={this.props.setMyLessonsChoiceToMy} text="Click to View My Lessons" />
