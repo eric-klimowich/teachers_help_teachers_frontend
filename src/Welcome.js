b6fa0277c8691d59623b72ca35679f0ae548ae24
eric-klimowich <eklimowich@hotmail.com>
2019-06-07 15:06:07 -0400

    Changed class names to improve understanding and readability of Welcome page

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Welcome = props => {
   return (
     <div>
-      <div id="welcome-intro" className="homepage-introduction-container" >
-        <h1 className="chalkboard-writing title">
+      <div id="welcome-intro" className="homepage-introduction__container" >
+        <h1 className="homepage-introduction__title">
           Welcome to Teachers Helping Teachers!
           <br />
           <br />
-          <h3 className="chalkboard-writing intro" >
+          <h3 className="homepage-introduction__intro" >
           A way for all teachers, new and experienced, to share and find lesson plan and worksheet samples for free.
           Since we are not only teachers but also lifelong students, helpful feedback is always welcome.
           I hope this serves as a valuable resource to the teaching community.
