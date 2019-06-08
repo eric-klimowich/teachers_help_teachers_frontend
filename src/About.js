551b89878aff177c842664a7c10b43bb144a47bd
eric-klimowich <eklimowich@hotmail.com>
2019-06-08 19:08:40 -0400

    Fixed nesting error on About page, improved structure

--- src/About.js
+++ src/About.js
@@ -8,19 +8,14 @@ class About extends Component {
   render() {
     return (
       <div>
-      <div>
-        <div id="welcome-intro" className="homepage-introduction-container" >
-            <h3 className="chalkboard-writing" >
-            As a new teacher just starting out in the profession, a great deal of my time and effort was spent creating lesson plans. Coming from a background as a not-for-profit program director, a philosophy I had adopted and always put to use was not "reinventing the wheel". I employed this same philosophy when it came to writing my lesson plans, always looking for ideas or techniques that I could adapt to my classrooms and students. However, I found that a lot of the plentiful resourses came at a price, literally.
-            <br />
-            <br />
-            <h3 className="chalkboard-writing" >
-            I created this site as a way for teachers, new and veteran alike, to freely share their ideas, experiences, and resources. I have found that most teachers spend enough of their own money on supplies, the same shouldn't have to be said for ideas. Teachers are very caring and giving, and this platform provides them with a place in which they can all work together as one community.
-            </h3>
-            </h3>
+        <div id="welcome-intro" className="about-introduction__container" >
+            <p className="about-introduction__p-one" >
+              As a new teacher just starting out in the profession, a great deal of my time and effort was spent creating lesson plans. Coming from a background as a not-for-profit program director, a philosophy I had adopted and always put to use was not "reinventing the wheel". I employed this same philosophy when it came to writing my lesson plans, always looking for ideas or techniques that I could adapt to my classrooms and students. However, I found that a lot of the plentiful resourses came at a price, literally.
+            </p>
+            <p className="about-introduction__p-two" >
+              I created this site as a way for teachers, new and veteran alike, to freely share their ideas, experiences, and resources. I have found that most teachers spend enough of their own money on supplies, the same shouldn't have to be said for ideas. Teachers are very caring and giving, and this platform provides them with a place in which they can all work together as one community.
+            </p>
         </div>
-      </div>
-        <br />
         <Button action={this.props.hideAboutPage} text="Back" />
       </div>
     )
